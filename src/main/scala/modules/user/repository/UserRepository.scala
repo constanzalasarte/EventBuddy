e5284b776de6aad81724b5179336bc70b04008bc
constanzalasarte <constanza.lasarte@ing.austral.edu.ar>
2024-05-15 10:47:09 -0300

    add parenthesis in user repo

--- src/main/scala/modules/user/repository/UserRepository.scala
+++ src/main/scala/modules/user/repository/UserRepository.scala
@@ -6,7 +6,7 @@ import scala.concurrent.Future
 
 trait UserRepository {
   def addUser(user: User): Future[Unit]
-  def getUsers: Future[Set[User]]
+  def getUsers(): Future[Set[User]]
   def updateUser(id: Int, newUser: User): Future[Unit]
   def byID(id: Int): Future[Option[User]]
   def deleteById(id: Int): Future[Boolean]
